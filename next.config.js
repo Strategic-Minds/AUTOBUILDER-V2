608504458009f45204095a57b995d7e858967d69
xps-admin <strategicmindsadvisory@gmail.com>
2026-07-11 22:38:59 -0400

    feat: migrate globals.css and config from uploaded design

--- next.config.js
+++ next.config.js
@@ -1,12 +1,26 @@
+const withPWA = require("@ducanh2912/next-pwa")({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  disable: process.env.NODE_ENV === "development",
+  workboxOptions: { disableDevLogs: true },
+});
+
 /** @type {import("next").NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "**" },
+    ],
+  },
   typescript: {
     ignoreBuildErrors: false,
   },
   eslint: {
     ignoreDuringBuilds: false,
   },
-}
+};
 
-module.exports = nextConfig
-
+module.exports = withPWA(nextConfig);
